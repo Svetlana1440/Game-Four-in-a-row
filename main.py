65c6ab2679f5f5dca9bcdbcea411e9262a1981ff
Svetlana Rudova <rudovasvetlana570@gmail.com>
2024-12-23 16:09:13 +0800

    Changes

--- main.py
+++ main.py
@@ -109,7 +109,7 @@ def winning_move(board, piece):
 
     # Проверка отрицательных наклонных диагоналей
     for c in range(COLUMN_COUNT - 3):
-        for r in range(ROW_COUNT - 3):
+        for r in range(3, ROW_COUNT):
             if board[r][c] == piece and board[r - 1][c + 1] == piece and board[r - 2][c + 2] == piece and board[r - 3][c + 3] == piece:
                 return True
 
